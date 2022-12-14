9d3b4e2c9504b44561acfb0ec90053316f66a38c
Shan Jiang <jiangshan789123@hotmail.com>
2022-12-14 14:17:12 -0500

    Add customized udp sender and receiver

---
+++ JUCE udpReceiver/ShanPlugin1101/Builds/MacOSX/build/ShanPlugin1101.build/Debug/ShanPlugin1101 - Shared Code.build/Objects-normal/arm64/include_juce_audio_plugin_client_ARA.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/shanjiang/Desktop/juce_test/1102/shan1101/ShanPlugin1101/JuceLibraryCode/include_juce_audio_plugin_client_ARA.cpp \
+  /Users/shanjiang/Downloads/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_ARA.cpp \
+  /Users/shanjiang/Downloads/JUCE/modules/juce_audio_plugin_client/ARA/juce_ARA_Wrapper.cpp \
+  /Users/shanjiang/Downloads/JUCE/modules/juce_core/system/juce_TargetPlatform.h \
+  /Users/shanjiang/Downloads/JUCE/modules/juce_audio_plugin_client/ARA/../utility/juce_CheckSettingMacros.h
---
+++ JUCE udpReceiver/ShanPlugin1101/Builds/MacOSX/build/ShanPlugin1101.build/Debug/ShanPlugin1101 - Shared Code.build/Objects-normal/arm64/include_juce_audio_processors_lv2_libs.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/shanjiang/Desktop/juce_test/1102/shan1101/ShanPlugin1101/JuceLibraryCode/include_juce_audio_processors_lv2_libs.cpp \
+  /Users/shanjiang/Downloads/JUCE/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp
